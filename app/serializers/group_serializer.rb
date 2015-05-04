36f78d048f19dc8435efc3505e067b962cffd8e2
fukuiretu <s0232101@gmail.com>
2015-05-04 22:30:05 +0900

    Add topic serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  membar_max_num :integer
+#  topic_max_num  :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :name
 end
