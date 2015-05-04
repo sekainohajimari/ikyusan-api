686df7b9955b2e5e0b50f7cbad346ae3c277e4f7
fukuiretu <s0232101@gmail.com>
2015-05-05 00:56:16 +0900

    Add column likes count

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,13 +2,14 @@
 #
 # Table name: ideas
 #
-#  id         :integer          not null, primary key
-#  topic_id   :integer
-#  poster_id  :integer
-#  content    :string(255)
-#  anonymity  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  topic_id    :integer
+#  poster_id   :integer
+#  content     :string(255)
+#  anonymity   :integer
+#  likes_count :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 # Indexes
 #
--- app/serializers/idea_serializer.rb
+++ app/serializers/idea_serializer.rb
@@ -1,5 +1,23 @@
+# == Schema Information
+#
+# Table name: ideas
+#
+#  id          :integer          not null, primary key
+#  topic_id    :integer
+#  poster_id   :integer
+#  content     :string(255)
+#  anonymity   :integer
+#  likes_count :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_ideas_on_topic_id  (topic_id)
+#
+
 class IdeaSerializer < ActiveModel::Serializer
-  attributes :id, :content, :post_user
+  attributes :id, :content, :likes_count, :post_user
 
   has_one :post_user
 end
