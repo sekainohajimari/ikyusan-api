9ca91e4bfbb76a36b14be6ccf311b3730bd88c9b
fukuiretu <s0232101@gmail.com>
2015-05-05 22:01:27 +0900

    Modify edit method

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -5,9 +5,11 @@ class Api::V1::ProfileController < ApplicationController
 
   def edit
     current_user.profile.update!(
-      :display_id,
-      :display_name
+      profile_params[:display_id],
+      profile_params[:display_name]
     )
+
+    render json: current_user.profile
   end
 
   def profile_params
