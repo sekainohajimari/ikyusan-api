c117a4a66b25a756102a21ceacf9606ecb8cc9f4
fukuiretu <s0232101@gmail.com>
2015-05-10 22:15:20 +0900

    Fix notification api

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,6 +1,6 @@
-ActiveSupport.on_load(:active_model_serializers) do
-  # Disable for all serializers (except ArraySerializer)
-  ActiveModel::Serializer.root = false
-  # Disable for ArraySerializer
-  ActiveModel::ArraySerializer.root = false
-end
+# ActiveSupport.on_load(:active_model_serializers) do
+#   # Disable for all serializers (except ArraySerializer)
+#   ActiveModel::Serializer.root = true
+#   # Disable for ArraySerializer
+#   ActiveModel::ArraySerializer.root = true
+# end
