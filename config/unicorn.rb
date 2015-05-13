c08015a95e5d56ad37dddf6ed1e45083466ef1f3
fukuiretu <s0232101@gmail.com>
2015-05-13 09:32:35 +0900

    Add cap

---
+++ config/unicorn.rb
@@ -0,0 +1,36 @@
+application = 'ikyusan-api'
+app_dir = File.expand_path('../../', __FILE__)
+
+working_directory app_dir
+
+listen "#{app_dir}/tmp/sockets/#{application}_unicorn.sock"
+pid "#{app_dir}/tmp/pids/#{application}_unicorn.pid"
+stdout_path "/var/log/rails/#{application}_unicorn.log"
+stderr_path "/var/log/rails/#{application}_unicorn.log"
+
+cpu_num = `cat /proc/cpuinfo | grep processor`.split("\n").size
+
+worker_processes 3 * cpu_num
+timeout 600
+preload_app true
+
+before_fork do |server, _worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+      nil
+    end
+  end
+end
+
+after_fork do |_server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
