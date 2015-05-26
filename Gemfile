d333dcc34987ebd89535af46ac7124b4d07babef
fukuiretu <s0232101@gmail.com>
2015-05-26 09:45:46 +0900

    Add gem and util class

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'omniauth-twitter'
 gem 'global'
 gem 'ridgepole'
 gem 'aasm'
+gem 'request_store'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -10,4 +10,11 @@ class Api::V1::ApplicationController < ActionController::API
   # project local modules
   include Authenticater
   include ExceptionHandler
+
+  before_action :store_request
+
+  private
+  def store_request
+    RequestStore.store[:request] = request
+  end
 end
