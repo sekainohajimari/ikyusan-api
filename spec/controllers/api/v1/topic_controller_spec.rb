524df2f322367c22e16b2269f2119ac9851e41b4
fukuiretu <s0232101@gmail.com>
2015-06-13 21:59:15 +0900

    Add create spec

--- spec/controllers/api/v1/topic_controller_spec.rb
+++ spec/controllers/api/v1/topic_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Topic resource', type: :request, autodoc: true do
   let!(:group) { create(:group) }
   let!(:group_member) { create(:group_member, :owner, group: group, user: current_user) }
-  let!(:topic) { create(:topic, group: group, build_user: current_user) }
 
   describe "GET /api/v1/g/:group_id/t" do
     let!(:group_id) { group.id }
+    let!(:topic) { create(:topic, group: group, build_user: current_user) }
 
     before do
       group_member
@@ -28,6 +28,27 @@ describe 'Topic resource', type: :request, autodoc: true do
         expect(body).to be_json_eql(current_user.id.to_json).at_path('topics/0/build_user/id')
         expect(body).to be_json_eql(current_user.profile.display_id.to_json).at_path('topics/0/build_user/profile/display_id')
         expect(body).to be_json_eql(current_user.profile.display_name.to_json).at_path('topics/0/build_user/profile/display_name')
+      end
+    end
+  end
+
+  describe "POST /api/v1/g/:group_id/t" do
+    let!(:group_id) { group.id }
+    let!(:params) { { name: 'hoge' } }
+
+    before do
+      group_member
+    end
+
+    context_user_authenticated do
+      it 'success' do
+        is_expected.to eq 200
+        body = response.body
+
+        expect(body).to have_json_path('topic')
+        expect(body).to have_json_path('topic/id')
+        expect(body).to have_json_path('topic/idea_max_num')
+        expect(body).to be_json_eql('hoge'.to_json).at_path('topic/name')
       end
     end
   end
