f59c3728b6c11507828aca53d7893c11ad529279
fukuiretu <s0232101@gmail.com>
2015-06-17 00:01:04 +0900

    Add edit and enable api spec

--- spec/controllers/api/v1/profile_controller_spec.rb
+++ spec/controllers/api/v1/profile_controller_spec.rb
@@ -14,4 +14,62 @@ describe 'Profile resource', type: :request, autodoc: true do
       end
     end
   end
+
+  describe "GET /api/v1/profile/edit" do
+    context_user_authenticated do
+      context 'when params display_name only' do
+        let!(:params) { { display_name: 'ほげ' } }
+
+        it 'success' do
+          is_expected.to eq 200
+          body = response.body
+
+          expect(body).to have_json_path('profile')
+          expect(body).to be_json_eql(profile.display_id.to_json).at_path('profile/display_id')
+          expect(body).to be_json_eql(params[:display_name].to_json).at_path('profile/display_name')
+          expect(body).to be_json_eql(profile.icon_url.to_json).at_path('profile/icon_url')
+        end
+      end
+
+      context 'when params display_name and display_id' do
+        let!(:params) { { display_name: 'ほげ', display_id: 'hoge' } }
+
+        it 'success' do
+          is_expected.to eq 200
+          body = response.body
+
+          expect(body).to have_json_path('profile')
+          expect(body).to be_json_eql(params[:display_id].to_json).at_path('profile/display_id')
+          expect(body).to be_json_eql(params[:display_name].to_json).at_path('profile/display_name')
+          expect(body).to be_json_eql(profile.icon_url.to_json).at_path('profile/icon_url')
+        end
+      end
+    end
+  end
+
+  describe "GET /api/v1/profile/enabled" do
+    context_user_authenticated do
+      context 'not exists display_id' do
+        let!(:params) { { display_id: "#{profile.display_id}-dummy" } }
+
+        it 'success' do
+          is_expected.to eq 200
+          body = response.body
+
+          expect(body).to be_json_eql(true.to_json).at_path('enabled')
+        end
+      end
+
+      context 'not exists display_id' do
+        let!(:params) { { display_id: "#{profile.display_id}" } }
+
+        it 'success' do
+          is_expected.to eq 200
+          body = response.body
+
+          expect(body).to be_json_eql(false.to_json).at_path('enabled')
+        end
+      end
+    end
+  end
 end
