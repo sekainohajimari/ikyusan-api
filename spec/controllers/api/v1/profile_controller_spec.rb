47dc426b6909a10b3a41f29686cc741cc763a72a
fukuiretu <s0232101@gmail.com>
2015-08-22 21:52:31 +0900

    fix issue 191

--- spec/controllers/api/v1/profile_controller_spec.rb
+++ spec/controllers/api/v1/profile_controller_spec.rb
@@ -48,7 +48,7 @@ describe 'Profile resource', type: :request, autodoc: true do
         end
       end
 
-      context 'when params display_name and display_id and apply_default_icon' do
+      context 'when params display_name and display_id and apply_default_icon true' do
         let!(:params) { { display_name: 'ほげ', display_id: 'hoge', apply_default_icon: 1 } }
 
         it 'success' do
@@ -60,6 +60,21 @@ describe 'Profile resource', type: :request, autodoc: true do
           expect(body).to be_json_eql(params[:display_name].to_json).at_path('profile/display_name')
           expect(body).to be_json_eql(Global.profile.default_icon_url.to_json).at_path('profile/icon_url')
           expect(body).to be_json_eql(true.to_json).at_path('profile/in_use_default_icon')
+        end
+      end
+
+      context 'when params display_name and display_id and apply_default_icon false' do
+        let!(:params) { { display_name: 'ほげ', display_id: 'hoge', apply_default_icon: 0 } }
+
+        it 'success' do
+          is_expected.to eq 200
+          body = response.body
+
+          expect(body).to have_json_path('profile')
+          expect(body).to be_json_eql(params[:display_id].to_json).at_path('profile/display_id')
+          expect(body).to be_json_eql(params[:display_name].to_json).at_path('profile/display_name')
+          expect(body).to be_json_eql(profile.icon_url.to_json).at_path('profile/icon_url')
+          expect(body).to be_json_eql(false.to_json).at_path('profile/in_use_default_icon')
         end
       end
     end
