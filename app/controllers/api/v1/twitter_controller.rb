b19351abfadc8704ad073d3a9e4631bac4345e4e
fukuiretu <s0232101@gmail.com>
2015-10-28 20:19:43 +0900

    Add api

--- app/controllers/api/v1/twitter_controller.rb
+++ app/controllers/api/v1/twitter_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::TwitterController < Api::V1::ApplicationController
   def tweet
-    client.update(twitter_params[:message])
+    message = twitter_params[:message]
+    if message.blank?
+      raise Error::ApiError.new('Request parameter invalid', 400)
+    end
+
+    client.update(message)
 
     head :no_content
   end
@@ -9,8 +14,8 @@ class Api::V1::TwitterController < Api::V1::ApplicationController
 
     def client
       @client ||= Twitter::REST::Client.new(
-        consumer_key: consumer_key,
-        consumer_secret: consumer_secret,
+        consumer_key: Global.social.twitter.key,
+        consumer_secret: Global.social.twitter.secret,
         access_token: current_user.oauth_token,
         access_token_secret: current_user.oauth_token_secret
       )
